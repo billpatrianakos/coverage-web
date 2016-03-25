e04bdd390c113579bb57d207b3a3609617e3f0bb
Bill <bill@billpatrianakos.me>
2016-03-24 20:57:31 -0500

    Scaffolded out basic server start script
    Just added some comments to myself so I know what to do next.

---
+++ server/app.js
@@ -0,0 +1,14 @@
+// Server startup script
+// =====================
+// Responsible for configuring the server,
+// inserting middleware, and starting the
+// app up. 
+//
+// Note: this was written on my iPhone, please excuse typos
+
+var express = require('express'),
+    app     = express().
+    
+// Middleware
+// ----------
+// Insert, configure, update middleware
