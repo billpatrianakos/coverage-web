35990618814b4ba51687950d32ff85cfed1d0a46
Bill <bill@billpatrianakos.me>
2016-03-27 22:11:06 -0500

    Update main app config to use certain settings in dev and test envs. Added configuration to use for title meta tags for each page.

---
+++ server/config/seo.js
@@ -0,0 +1,8 @@
+module.exports = {
+  quotes: {
+    index: {
+      title: 'Get the right health insurance plan for you and your family | Coverage',
+      description: 'Coverage quoting lets you find plans based on your family\'s unique situation'
+    }
+  }
+}
