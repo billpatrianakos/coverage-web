23f2fa6c18aa57c9211d0afc58a374544700639c
Bill <bill@billpatrianakos.me>
2016-03-27 22:12:17 -0500

    Update main app file to use Handlebars as view engine.

--- server/app.js
+++ server/app.js
@@ -6,9 +6,36 @@
 //
 // Note: this was written on my iPhone, please excuse typos
 
-var express = require('express'),
-    app     = express();
+'use strict'
+
+let express = require('express'),
+    exphbs  = require('express-handlebars'),
+    app     = express(),
+    _       = require('lodash'),
+    config  = _.merge(require(__dirname + '/config/app').common, require(__dirname + '/config/app')[process.env.NODE_ENV]);
+
+
+// App configuration
+// -----------------
+// Configure views and other settings
+app.set('hbs', exphbs(config.handlebars));
+app.set('view engine', 'hbs');
+
     
 // Middleware
 // ----------
 // Insert, configure, update middleware
+
+
+// Routes
+// ------
+// Initialize user facing routes
+require('./controllers')(app);
+
+
+// Start server
+// ------------
+// Start the server
+let server = app.listen(process.env.PORT, () => {
+  console.log('Started app on localhost:' + server.address().port);
+});
