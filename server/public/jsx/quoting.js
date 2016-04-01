8038d4515cfae40f9262a7d30daa2086b85d0ae3
Bill <bill@billpatrianakos.me>
2016-04-01 18:19:29 -0500

    Add react components (compiled and JSX templates.

--- server/public/jsx/quoting.js
+++ server/public/jsx/quoting.js
@@ -7,14 +7,32 @@ var React     = require('react'),
 // The container component
 // for the entire page
 var QuotingContainer = React.createClass({
+  getInitialState: function() {
+    return {
+      plans: []
+    };
+  },
   render: function() {
     return (
       <div className="container">
         <div className="row-loose">
-          <div className="col-4">
-            Sidebar
+          <div className="col-3">
+            <div className="form-sidebar">
+              <h3 className="sans">Tell us about yourself</h3>
+              <form method="post" action="/quotes/">
+                <label><input type="text" name="zip_code" placeholder="Zip code" /></label>
+                <label><input type="text" name="age" placeholder="Your age" /></label>
+                <label><input type="text" name="household_income" placeholder="Household income" /></label>
+                <div className="checkbox-holder">
+                  <span className="label">Do you smoke?</span>
+                  <label className="inline">No <input type="radio" name="tobacco_use" value="0" /></label>
+                  <label className="inline">Yes <input type="radio" name="tobacco_use" value="1" /></label>
+                </div>
+                <a href="#" className="btn">Add Dependents</a>
+              </form>
+            </div>
           </div>
-          <QuoteResults results={[]} />
+          <QuoteResults results={this.state.plans} />
         </div>
       </div>
     );
